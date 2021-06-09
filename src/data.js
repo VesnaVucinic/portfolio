d9be58b9ba50118c2c0c769e9d9669922600f624
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-06-09 18:32:39 +0100

    add data file

---
+++ src/data.js
@@ -0,0 +1,6 @@
+const projects = [
+    {url: "https://www.youtube.com/watch?v=YHXcY13bUwE&t=53s", name: "React-Redux Project"},
+    {url: "https://www.youtube.com/watch?v=iHoqoSCiVcA&t=11s", name: "JavaScript Project"},
+    {url: "https://www.youtube.com/watch?v=RVN4pmcJtF8&t=19s", name: "Ruby on Rails Project"},
+    {url: "https://www.youtube.com/watch?v=-q7OTzbdKU0&t=4s", name: "Sinatra Project"}
+]
