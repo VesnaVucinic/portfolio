0ee840e0c6c76ac44748f655632652b38418050b
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-06-12 20:27:25 +0100

    add id for project

--- src/data.js
+++ src/data.js
@@ -1,8 +1,10 @@
+let counter = 1
+
 const projects = [
-    {url: "https://www.youtube.com/watch?v=YHXcY13bUwE&t=53s", name: "React-Redux Project"},
-    {url: "https://www.youtube.com/watch?v=iHoqoSCiVcA&t=11s", name: "JavaScript Project"},
-    {url: "https://www.youtube.com/watch?v=RVN4pmcJtF8&t=19s", name: "Ruby on Rails Project"},
-    {url: "https://www.youtube.com/watch?v=-q7OTzbdKU0&t=4s", name: "Sinatra Project"}
+    {id: counter++, url: "https://www.youtube.com/watch?v=YHXcY13bUwE&t=53s", name: "React-Redux Project"},
+    {id: counter++, url: "https://www.youtube.com/watch?v=iHoqoSCiVcA&t=11s", name: "JavaScript Project"},
+    {id: counter++, url: "https://www.youtube.com/watch?v=RVN4pmcJtF8&t=19s", name: "Ruby on Rails Project"},
+    {id: counter++, url: "https://www.youtube.com/watch?v=-q7OTzbdKU0&t=4s", name: "Sinatra Project"}
 ]
 
 export default projects
